bae7743d698d99b225b5fed7cbba2597ce78ccac
Carlos Rojas <c2rojas75@gmail.com>
2020-02-20 01:58:13 -0800

    playing with substring

--- employee-directory-db/src/utils/API.js
+++ employee-directory-db/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default {
   // Gets all users
-  getUsers: function() {
+  getUsers: function () {
     return new Promise((resolve, reject) => {
       axios
         .get("https://randomuser.me/api/?results=200&nat=us")
@@ -22,37 +22,42 @@ export default {
         .catch(err => reject(err));
     });
   },
-  // getUser: function(search) {
-  //   return new Promise((resolve, reject) => {
-  //     axios
-  //       .get("https://randomuser.me/api/?results=200&nat=us")
-  //       .then(res => {
-  //         const users = res.data.results;
-  //         const data = users
-  //         .filter(check => {
-  //           var stringName = check.name.first+check.name.last;
-  //           var length = search.trim().length
-  //           var boo = false
-  //           for(var i = 0; i<=stringName.length-1; i++){
-  //             for(var j=0; i<=stringName.length-1-length; j++) {
-  //               if()
-  //             }
-  //           }
+  getUser: function (search) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get("https://randomuser.me/api/?results=200&nat=us")
+        .then(res => {
+          const users = res.data.results;
+          const data = users
+            .filter(check => {
+              var stringName = check.name.first + check.name.last;
+              var length = search.trim().length
+              var boo = false
+              var searchingString = ''
+              for (var i = 0; i <= stringName.length - 1 - length; i++) {
+                
+                if(stringName.substring(i, i+length) === search)
+                return
+                // for (var k = i; k <= i+length - 1; k++) {
+                //   searchingString += stringName[k]
+                // }
+              }
+              return searchingString === search.trim()
 
-  //         })
-  //         .map(user => {
-  //           return {
-  //             image: user.picture.thumbnail,
-  //             first: user.name.first,
-  //             last: user.name.last,
-  //             phone: user.phone,
-  //             email: user.email,
-  //             dob: user.dob.date
-  //           };
-  //         });
-  //         resolve(data);
-  //       })
-  //       .catch(err => reject(err));
-  //   });
-  // }
+            })
+            .map(user => {
+              return {
+                image: user.picture.thumbnail,
+                first: user.name.first,
+                last: user.name.last,
+                phone: user.phone,
+                email: user.email,
+                dob: user.dob.date
+              };
+            });
+          resolve(data);
+        })
+        .catch(err => reject(err));
+    });
+  }
 };
